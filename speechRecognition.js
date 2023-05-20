3e7bd648edf5d151b580f4b263bc4f375e36f1ab
Yash Vora <vorayash9028@gmail.com>
2023-05-20 13:42:26 +0530

    first commit

---
+++ speechRecognition.js
@@ -0,0 +1,50 @@
+if ("webkitSpeechRecognition" in window) {
+  let speechRecognition = new webkitSpeechRecognition();
+  let final_transcript = "";
+  let stop = false;
+
+  speechRecognition.continuous = true;
+  speechRecognition.interimResults = true;
+  speechRecognition.lang = 'gu-IN';
+  console.log(speechRecognition.lang);
+  console.log("hii");
+  speechRecognition.onstart = () => {
+    document.querySelector("#status").style.display = "block";
+  };
+  speechRecognition.onerror = () => {
+    document.querySelector("#status").style.display = "none";
+    console.log("Speech Recognition Error");
+  };
+  speechRecognition.onend = () => {
+    document.querySelector("#status").style.display = "none";
+    console.log("Speech Recognition Ended");
+    if(!stop) {
+      speechRecognition.start();
+    }
+  };
+
+  speechRecognition.onresult = (event) => {
+    let interim_transcript = "";
+
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+    document.querySelector("#final").innerHTML = final_transcript;
+    document.querySelector("#interim").innerHTML = interim_transcript;
+  };
+
+  document.querySelector("#start").onclick = () => {
+    stop = false;
+    speechRecognition.start();
+  };
+  document.querySelector("#stop").onclick = () => {
+    stop = true;
+    speechRecognition.stop();
+  };
+} else {
+  console.log("Speech Recognition Not Available");
+}
